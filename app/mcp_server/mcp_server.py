55668c5b06cc2f8430f1a3977e0c3b88c3ec6e9c
Eiscreme <eiscreme@Mac.fritz.box>
2025-04-27 01:11:05 +0200

    add google auth

--- app/mcp_server/mcp_server.py
+++ app/mcp_server/mcp_server.py
@@ -46,7 +46,11 @@ oauth.register(
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile'},
+    client_kwargs={
+        'scope': 'openid email profile',
+        'redirect_uri': REDIRECT_URI  # Explicitly set
+    },
+
 )
 
 # Dummy user database
